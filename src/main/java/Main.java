5580853b010a6fc1b5d8021ef62592acd42adb01
Radosław Wróbel <161605563+RadoslawWrobel@users.noreply.github.com>
2024-06-19 12:12:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+// Kolokwium zaliczeniowe myk pyk
+import java.util.Scanner;
+
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("Hello world!");
+  }
+
+}
