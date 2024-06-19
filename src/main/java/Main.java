3c6af57e7be33f275c2e51030211404898b079d6
Radosław Wróbel <161605563+RadoslawWrobel@users.noreply.github.com>
2024-06-19 12:28:59 +0000

    Zaliczenie na 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 // Kolokwium zaliczeniowe myk pyk
+/* Napisz program, który wypisuje długość danego łańcucha znaków:
+- Program powinien wczytać łańcuch znaków wprowadzony przez użytkownika.
+- Obliczyć długość łańcucha.
+- Wyświetlić wynik.
+  */
 import java.util.Scanner;
 
 public class Main 
 {
   public static void main(String[] args) 
   {
-    System.out.println("Hello world!");
+    Scanner SkanerZaliczeniowy = new Scanner(System.in);
+    System.out.println("Podaj tekst: ");
+    String NowyTekst = SkanerZaliczeniowy.nextLine();
+
+    System.out.println("Długość tekstu to: " + NowyTekst.length());
   }
 
 }
